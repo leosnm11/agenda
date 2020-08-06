a9cbe08e29974dddabaee8e3f22fbb53293ab8b2
leosnm11 <leandro_cmb@hotmail.com>
2020-08-06 14:21:49 -0300

    Corrigindo apontamento da url

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://app-agenta-flux.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
