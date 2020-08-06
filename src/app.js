9722db837bc3a40e92107028b71bb98cf39eb726
leosnm11 <leandro_cmb@hotmail.com>
2020-08-06 14:08:04 -0300

    Corrigindo aprontamento do build

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const { connection } = mongoose;
 db;
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/v1/api/agenda', routes);
 
 connection.once('open', () => {
